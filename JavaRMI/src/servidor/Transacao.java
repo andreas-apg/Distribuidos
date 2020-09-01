d041f40632f82d4e8216410f907381c19d5e19b2
Andreas <andreasp.gomes@gmail.com>
2020-09-01 19:04:29 -0300

    Populated Ordem and Transacao.

--- JavaRMI/src/servidor/Transacao.java
+++ JavaRMI/src/servidor/Transacao.java
@@ -1,9 +1,21 @@
 package servidor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+/* classe para a transação de uma ação.
+ * Para saber a cotação de uma ação,
+ * pegar o objeto tipo Transacao pra
+ * uma determinada acao e olha
+ * o que possui a hora mais recente.
+ */ 
 public class Transacao {
-
-	public Transacao() {
-		// TODO Auto-generated constructor stub
-	}
-
+	String vendedor; 	// usuário
+	String comprador; 	// usuário
+	String acao; 		// ie PETR4
+	float preco; 		// em reais
+	int quantidade; 	// número inteiro 
+	LocalDateTime hora;
+	
+	public static ArrayList<Transacao> transacoes;
 }
