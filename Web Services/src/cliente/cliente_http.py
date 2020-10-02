b7644c30d55e9d8838b800af5a34fb260d43194d
Giovane <giovanenegrini@gmail.com>
2020-10-01 22:27:51 -0300

    add SSE service to both client and server

--- Web Services/src/cliente/cliente_http.py
+++ Web Services/src/cliente/cliente_http.py
@@ -4,15 +4,37 @@ from dataclasses import asdict
 from helpers import Helpers
 from loggable import Loggable
 from exceptions.server_exception import ServerException
+import threading
+import sseclient
+import pprint
 
-class ClienteHttp(Loggable):
+class ClienteHttp(Loggable,threading.Thread):
     """Classe responsavel por enviar as mensagens para o servidor de acoes"""
 
     headers = { 'Content-Type': "application/json" }
-    server_url: str = "http://localhost:8080/api"
+    server_url: str = "http://localhost:8080/"
     server_endpoints: dict = {
-        "ordem": "/ordem"
+        "notificacao": "events/notificacao",
+        "ordem": "api/ordem"
     }
+    
+    def __init__(self, username):
+        threading.Thread.__init__(self, daemon=True)
+        self.username = username
+
+    def run(self):
+        self.servico_de_notificacao()
+
+    def servico_de_notificacao(self):
+        request_url: str = ClienteHttp.server_url + ClienteHttp.server_endpoints['notificacao']
+        payload: dict = {'username':self.username}
+        print ("Iniciando servico de notificacao ")
+        
+        response = requests.get(request_url, params=payload, stream=True)
+        client = sseclient.SSEClient(response)
+        for event in client.events():
+            pprint.pprint(event.data)
+        print ("Encerrando servico de notificacao")
     
 
     def emitir_ordem(self, ordem:Ordem) -> None:
--- Web Services/src/cliente/main_cliente.py
+++ Web Services/src/cliente/main_cliente.py
@@ -1,17 +1,20 @@
 from menu_cliente import MenuCliente
 from cliente_http import ClienteHttp
+import time
 
 class MainCliente:
     """Initial class for project with the main function"""
 
     
-
-    def run(self):
+    def main(self):
 
             self.username: str = "john snow"
             server_url: str
 
-            self.cliente:ClienteHttp = ClienteHttp()
+            
+            self.cliente: ClienteHttp = ClienteHttp(self.username)
+            self.cliente.start()
+            time.sleep(2)
         
             self.menu: MenuCliente = MenuCliente(self.username, self.cliente)
             self.menu.start_menu()
@@ -19,4 +22,4 @@ class MainCliente:
             print ("Hello, world!")
 
 if __name__ == '__main__':
-    MainCliente().run()
+    MainCliente().main()
